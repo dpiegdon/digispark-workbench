f14037c58a37ba7090fd3e24b8ab3c3d6c8232df
David R. Piegdon <dgit@piegdon.de>
2018-10-27 02:26:47 +0000

    twi implementation that passes along to ws2812

--- projects/example-ws2812-relay/main.c
+++ projects/example-ws2812-relay/main.c
@@ -5,18 +5,26 @@
 #include <util/delay.h>
 #include <avr/pgmspace.h>
 
-#define LED_COUNT 40
 
-uint8_t ledBuffer[3*LED_COUNT];
+#include "ws2812.h"
+#include "usi_twi.h"
+
+#define LED_COUNT 40
 
 int main(void)
 {
-	wdt_enable(WDTO_1S);
+	// power off all peripheral. all enabled on demand.
+	PRR = (1<<PRTIM1) | (1<<PRTIM0) | (1<<PRUSI) | (1<<PRADC);
+
+	wdt_enable(WDTO_2S);
+	usi_twi_init();
+	ws2812_init();
 
 	sei();
 
 	while(1) {
 		wdt_reset();
+		_delay_ms(250);
 	}
 }
 
