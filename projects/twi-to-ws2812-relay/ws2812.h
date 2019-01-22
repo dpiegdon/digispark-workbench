ec357f1840890777ec270a1111cbf72bac2a2681
David R. Piegdon <dgit@piegdon.de>
2019-01-22 22:07:41 +0000

    refactor ws2812 header for generic use

--- projects/twi-to-ws2812-relay/ws2812.h
+++ projects/twi-to-ws2812-relay/ws2812.h
@@ -2,19 +2,23 @@
 #ifndef __WS2812_H__
 # define __WS2812_H__
 
-#include <stdint.h>
-#include <avr/cpufunc.h>
-#include <avr/io.h>
+# include <stdint.h>
+# include <avr/cpufunc.h>
+# include <avr/io.h>
 
-#define PIN_LED PB1
-#define PORT_LED ((&PORTB)-__SFR_OFFSET)
+# ifndef PIN_WS2812_LED
+#  define PIN_WS2812_LED PB1
+# endif
+# ifndef PORT_WS2812_LED
+#  define PORT_WS2812_LED ((&PORTB)-__SFR_OFFSET)
+# endif
 
-// define this if your uC is running at 16mhz system clock
-//#define WS2812_16MHZ_CLOCK
+// Define "WS2812_8MHZ_CLOCK" if your uC is running at 8 MHz system clock.
+// Otherwise 16 MHz is assumed.
 
 static inline void ws2812_init(void)
 {
-	DDRB |= (1 << PIN_LED);
+	DDRB |= (1 << PIN_WS2812_LED);
 }
 
 static inline void ws2812_send_single_byte(uint8_t byte)
@@ -27,7 +31,7 @@ static inline void ws2812_send_single_byte(uint8_t byte)
 					     "nop \n\t"
 					     "nop \n\t"
 					     "nop \n\t"
-#ifdef WS2812_16MHZ_CLOCK
+# ifndef WS2812_8MHZ_CLOCK
 					     "nop \n\t"
 					     "nop \n\t"
 					     "nop \n\t"
@@ -36,24 +40,24 @@ static inline void ws2812_send_single_byte(uint8_t byte)
 					     "nop \n\t"
 					     "nop \n\t"
 					     "nop \n\t"
-#endif // WS2812_16MHZ_CLOCK
+# endif // WS28128_8MHZ_CLOCK
 					     "cbi %0, %1 \n\t"
 					     :
-					     : "i" (PORT_LED), "i" (PIN_LED)
+					     : "i" (PORT_WS2812_LED), "i" (PIN_WS2812_LED)
 					     :
 					);
 		} else {
 			__asm__ __volatile__("sbi %0, %1 \n\t"
-#ifdef WS2812_16MHZ_CLOCK
+# ifndef WS2812_8MHZ_CLOCK
 					     "nop \n\t"
 					     "nop \n\t"
-#endif // WS2812_16MHZ_CLOCK
+# endif // WS28128_8MHZ_CLOCK
 					     "nop \n\t"
 					     "cbi %0, %1 \n\t"
 					     "nop \n\t"
 					     "nop \n\t"
 					     :
-					     : "i" (PORT_LED), "i" (PIN_LED)
+					     : "i" (PORT_WS2812_LED), "i" (PIN_WS2812_LED)
 					     :
 					);
 		}
