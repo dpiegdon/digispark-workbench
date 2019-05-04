6df38197eb69b9ddb75287879557ddcc8c6244ef
David R. Piegdon <dgit@piegdon.de>
2019-05-04 00:54:17 +0000

    add five supported colors to knight rider

--- projects/ws2812-knight-rider/main.c
+++ projects/ws2812-knight-rider/main.c
@@ -13,6 +13,29 @@ static inline uint8_t attenuate(uint8_t value)
 	return v;
 }
 
+#define COLOR_PINK
+#if defined COLOR_RAL2011
+const uint8_t R = 0xFF;
+const uint8_t G = 0x61;
+const uint8_t B = 0x06;
+#elif defined COLOR_PINK
+const uint8_t R = 0xFF;
+const uint8_t G = 0x00;
+const uint8_t B = 0xFF;
+#elif defined COLOR_YELLOW
+const uint8_t R = 0xFF;
+const uint8_t G = 0xD0;
+const uint8_t B = 0x00;
+#elif defined COLOR_BLUE
+const uint8_t R = 0x00;
+const uint8_t G = 0x00;
+const uint8_t B = 0xFF;
+#elif defined COLOR_RED
+const uint8_t R = 0xFF;
+const uint8_t G = 0x00;
+const uint8_t B = 0x00;
+#endif
+
 int main(void)
 {
 	uint8_t i;
@@ -22,7 +45,7 @@ int main(void)
 	int8_t center = 0;
 	int8_t direction = 1;
 
-	uint8_t brightness[light_count];
+	uint16_t brightness[light_count];
 
 	// power off all peripheral. all enabled on demand.
 	PRR = (1<<PRTIM1) | (1<<PRTIM0) | (1<<PRUSI) | (1<<PRADC);
@@ -50,9 +73,12 @@ int main(void)
 
 		// set lights
 		for(i=0; i < light_count; ++i) {
-			ws2812_set_single(brightness[i], 0, brightness[i]);
-			ws2812_set_single(brightness[i], 0, brightness[i]);
-			ws2812_set_single(brightness[i], 0, brightness[i]);
+			uint16_t r = R * brightness[i] / 256;
+			uint16_t g = G * brightness[i] / 256;
+			uint16_t b = B * brightness[i] / 256;
+			ws2812_set_single(r, g, b);
+			ws2812_set_single(r, g, b);
+			ws2812_set_single(r, g, b);
 		}
 
 		// wait a bit
